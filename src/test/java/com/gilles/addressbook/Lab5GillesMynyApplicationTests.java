72a9d96bda59db0946a9838b97af7a11193f376a
Gilles Myny <gillesmyny@cmail.carleton.ca>
2023-02-07 13:02:37 -0500

    Fixed tests

--- src/test/java/com/gilles/addressbook/Lab5GillesMynyApplicationTests.java
+++ src/test/java/com/gilles/addressbook/Lab5GillesMynyApplicationTests.java
@@ -11,61 +11,61 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class Lab5GillesMynyApplicationTests {
+    @Value(value="${local.server.port}")
+    int port;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
-    private String buddiesResourceUrl = "http://localhost:8080/buddies";
-    private String addressBooksResourceUrl = "http://localhost:8080/addressbooks";
-
-
     @Test
     void getPlainBuddiesJSON() {
-        ResponseEntity<String> response = restTemplate.getForEntity(buddiesResourceUrl, String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/buddies", String.class);
         Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
     }
 
     @Test
     void getPlainAddressBooksJSON() {
-        ResponseEntity<String> response = restTemplate.getForEntity(addressBooksResourceUrl, String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/addressbooks", String.class);
         Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
     }
 
     @Test
     void getPlainBuddyInfo() {
-        BuddyInfo buddy = restTemplate.getForObject(buddiesResourceUrl + "/1", BuddyInfo.class);
+        BuddyInfo buddy = restTemplate.getForObject("http://localhost:" + port + "/buddies/1", BuddyInfo.class);
         Assertions.assertNotNull(buddy.getName());
         Assertions.assertEquals(buddy.getId(), 1);
     }
 
     @Test
     void getPlainAddressBook() {
-        AddressBook aBook = restTemplate.getForObject(addressBooksResourceUrl + "/1", AddressBook.class);
+        AddressBook aBook = restTemplate.getForObject("http://localhost:" + port + "/addressbooks/1", AddressBook.class);
         Assertions.assertNotNull((aBook.getBuddyList()));
         Assertions.assertEquals(aBook.getId(), 1);
     }
 
     @Test
     void testBuddyInfoHttpHeader() {
-        HttpHeaders httpBuddyHeaders = restTemplate.headForHeaders(buddiesResourceUrl);
+        HttpHeaders httpBuddyHeaders = restTemplate.headForHeaders("http://localhost:" + port + "/buddies");
         Assertions.assertTrue(httpBuddyHeaders.getContentType().includes(MediaType.APPLICATION_JSON));
     }
 
     @Test
     void testAddressInfoHttpHeader() {
-        HttpHeaders httpAddressHeaders = restTemplate.headForHeaders(addressBooksResourceUrl);
+        HttpHeaders httpAddressHeaders = restTemplate.headForHeaders("http://localhost:" + port + "/addressbooks");
         Assertions.assertTrue(httpAddressHeaders.getContentType().includes(MediaType.APPLICATION_JSON));
     }
 
     @Test
     void testAddressBookPost() {
         HttpEntity<AddressBook> request = new HttpEntity<>(new AddressBook());
-        AddressBook aBook = restTemplate.postForObject(addressBooksResourceUrl, request, AddressBook.class);
+        AddressBook aBook = restTemplate.postForObject("http://localhost:" + port + "/addressbooks", request, AddressBook.class);
         Assertions.assertNotNull(aBook);
         Assertions.assertEquals(aBook.getId(), 2);
     }
@@ -73,7 +73,7 @@ class Lab5GillesMynyApplicationTests {
     @Test
     void testBuddyInfoPost() {
         HttpEntity<BuddyInfo> request = new HttpEntity<>(new BuddyInfo("Gilles", "Ottawa", 767));
-        BuddyInfo buddy = restTemplate.postForObject(buddiesResourceUrl, request, BuddyInfo.class);
+        BuddyInfo buddy = restTemplate.postForObject("http://localhost:" + port + "/buddies", request, BuddyInfo.class);
         Assertions.assertNotNull(buddy);
         Assertions.assertEquals(buddy.getId(), 3);
     }
